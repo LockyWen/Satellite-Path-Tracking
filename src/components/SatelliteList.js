b8791a3a3a8b994cc0e92b1dac93f52775b23500
LockyWen <2846434229@qq.com>
2020-08-21 00:28:39 -0400

    stage changes

---
+++ src/components/SatelliteList.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import { List, Avatar, Button, Checkbox, Spin} from 'antd';
+import Satellite from "../assets/images/Satellite.svg";
+
+
+class SatelliteList extends Component{
+    render(){
+        const satList = this.props.satInfo ? this.props.satInfo.above : [];
+
+
+        return (
+            <div className="sat-list-box">
+                <Button className="sat-list-btn"
+                size="large">Track on the map</Button>
+                <hr />
+
+                {
+                    this.props.loading ?
+                    <Spin tip="Loading Satellites..." />:
+                    <List
+                        className="sat-list"
+                        itemLayout="horizontal"
+                        size="small"
+                        dataSource={satList}
+                        renderItem={item => (
+                            <List.Item
+                                actions={[<Checkbox dataInfo={item} onChange={this.onChange}/>]}
+                            >
+                                <List.Item.Meta
+                                    avatar={<Avatar size={50} src={Satellite} />}
+                                    title={<p>{item.satname}</p>}
+                                    description={`Launch Date: ${item.launchDate}`}
+                                />
+    
+                            </List.Item>
+                        )}
+                    />
+                        }
+                
+            </div>
+        );
+    }
+}
+
+export default SatelliteList;
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const SAT_BASE_URL = "https://www.n2yo.com/rest/v1/satellite";
+
+export const SAT_API_KEY = "J32NZS-VJTNP2-XVA2T3-4J0W"; // Don’t copy this, use the API license key you get from N2YO
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = `${SAT_BASE_URL}/above`;
